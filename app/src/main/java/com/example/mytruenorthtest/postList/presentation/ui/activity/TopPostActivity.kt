c5696ac6d3acf44ccc0c1f2e52536cb0c6ad18a2
Personal-Desktop <dagc_1496@hotmail.com>
2022-03-12 14:06:34 -0500

    User Experience is created

---
+++ app/src/main/java/com/example/mytruenorthtest/postList/presentation/ui/activity/TopPostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mytruenorthtest.postList.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mytruenorthtest.R
+
+class TopPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_post)
+    }
+}
