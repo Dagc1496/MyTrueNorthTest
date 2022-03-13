bcb5faf2e124583e55819dd8554c1ba53ba87035
Personal-Desktop <dagc_1496@hotmail.com>
2022-03-12 20:49:48 -0500

    Fix in headers for Query to get Pages from API

--- app/src/main/java/com/example/mytruenorthtest/postList/data/remote/network/TopApiClient.kt
+++ app/src/main/java/com/example/mytruenorthtest/postList/data/remote/network/TopApiClient.kt
@@ -1,9 +1,8 @@
 package com.example.mytruenorthtest.postList.data.remote.network
 
-import com.example.mytruenorthtest.postList.data.remote.dto.PostResponse
 import com.example.mytruenorthtest.postList.data.remote.dto.TopResponse
-import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface TopApiClient {
 
@@ -11,6 +10,6 @@ interface TopApiClient {
     suspend fun fetchTop(): TopResponse
 
     @POST("/top.json")
-    suspend fun fetchTopNextPage(@Header("after")after: String? = null): TopResponse
+    suspend fun fetchTopNextPage(@Query("after")after: String? = null): TopResponse
 
 }
